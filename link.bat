478cea97939c022a69c6a8558bc9c754edf6334a
Shoey <shoeyfiller@gmail.com>
2024-09-04 15:28:32 -0400

    updated batch file to handle more spaces

--- link.bat
+++ link.bat
@@ -1,21 +1,18 @@
 @echo off
 
-SET CurrentDirectory=%~dp0
-for %%B in (%CurrentDirectory%.) do set parent=%%~dpB
-for %%B in (%parent%.) do set grandparent=%%~dpB
 
-
-
-@echo Linking %1\common to %grandparent%saves\common.
+CD..
+@echo Linking %1\common to %CD%\saves\common.
 @echo Enter Y to confirm.
-set /p "confirm="
 
-if %confirm% == Y (
+
+set /p "confirm="
+ if %confirm% == Y (
 	if exist %1\common ( 
 		rename %1\common "commonbackup"
 		@echo Renamed existing common folder to commonbackup.
 	)
-	mklink /j %1\common %grandparent%saves\common
+	mklink /j %1\common "%CD%\saves\common"
 ) else (
 @echo Link cancelled.)
 pause
