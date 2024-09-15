8cd767b93aea8fdb9f5ccf870889fa941553ed68
Shoey <shoeyfiller@gmail.com>
2024-09-15 19:56:29 -0400

    renamed common message timer, set up for consecutive commands

--- src/Shoey/BetterSaves/ScriptSaveLoadCommonMissing.java
+++ src/Shoey/BetterSaves/ScriptSaveLoadCommonMissing.java
@@ -2,20 +2,23 @@ package Shoey.BetterSaves;
 
 import com.fs.starfarer.api.EveryFrameScript;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.SettingsAPI;
 
-public class CommonMessageTimer implements EveryFrameScript {
+import static Shoey.BetterSaves.MainPlugin.*;
+
+public class ScriptSaveLoadCommonMissing implements EveryFrameScript {
 
     float timer = 0;
-    boolean messageShown = false;
-
+    boolean done = false;
+    SettingsAPI settings = Global.getSettings();
     @Override
     public boolean isDone() {
-        return messageShown;
+        return done;
     }
 
     @Override
     public boolean runWhilePaused() {
-        return false;
+        return true;
     }
 
     @Override
@@ -24,7 +27,8 @@ public class CommonMessageTimer implements EveryFrameScript {
         if (timer > 2)
         {
             Global.getSector().getCampaignUI().addMessage("BetterSaves: Common settings redirect missing, recommend checking forum thread or readme for assistance.");
-            messageShown = true;
+            ReadyForCulling = true;
+            done = true;
         }
     }
 }
